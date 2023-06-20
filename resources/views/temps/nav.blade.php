177715ce6c2cdd0590f0d10d8414044a3d675cdd
ShannonKendi <shannon.kihara@gmail.com>
2023-06-20 21:48:04 +0300

    first commit

---
+++ resources/views/temps/nav.blade.php
@@ -0,0 +1,16 @@
+<nav class="main-nav w-100">
+    <div class="logo-div">
+        <img src="images/lllogo.png" alt="">
+    </div>
+
+    <div class="categories w-75">
+        <div class=""></div>
+        <a href="/"><button class="nav_btn w-100"><span class="hov"></span> HOME</button></a>
+        <button class="nav_btn"><span class="hov"></span> ABOUT</button>
+        <a href="register" class="w-100"> <button class="nav_btn w-100"><span class="hov"></span> ENROLL</button></a>
+
+        <div class="menu-div">
+            <button class="btn-text"><i class="fa-sharp fa-solid fa-shield-halved"></i></i></button>
+        </div>
+    </div>
+</nav>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+
+    <!-- Fontawesome icons cdn link -->
+    <script src="https://kit.fontawesome.com/db540a34d6.js" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/d08562217f.js" crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" href="css/index.css">
+    <title>ASSERT</title>
+</head>
+<body>
+<nav class="main-nav w-100">
+        <div class="logo-div">
+            <img src="images/lllogo.png" alt="">
+        </div>
+        <div class="categories w-75">
+            <button class="nav_btn"><span class="hov"></span> HOME</button>
+            <button class="nav_btn"><span class="hov"></span> ABOUT</button>
+            <a href="login" class="w-100"> <button class="nav_btn w-100"><span class="hov"></span> LOGIN</button></a>
+
+            <a href="register" class="w-100"> <button class="nav_btn w-100"><span class="hov"></span> REGISTER</button></a>
+            <div class="menu-div">
+                <button class="btn-text"><i class="fa-sharp fa-solid fa-shield-halved"></i></button>
+            </div>
+        </div>
+               
+                
+</nav>
+<section class="main w-100">
+   
+        <div class="hero-text">
+        <h1> ASSERT</h1>
+            <h2>We vouch for:</h2>
+            <h2>An all rounded education</h2>
+        </div>
+        <div class="hero-btns">
+            <a href="register"> <button class="hero-btn" role="button"><span class="text">ENROLL</span></button></a>
+            <button class="hero-btn" role="button"><span class="text">ABOUT US</span></button>
+        </div>
+    </section>
+    <section class="ad w-100">
+        <div class="text w-100">
+            <h2>Learn how to do things the best way</h2>
+            <h1>"Consistency and working smart is the key"</h1>
+            <p>-Julie Jones</p>
+        </div>
+        <div class="enrol-options">
+            <button class="btn-enrol-option">
+                <span class="hov-e"></span>
+                Diplomas
+            </button>
+            <button class="btn-enrol-option">
+                <span class="hov-e"></span>
+                Certificates
+            </button>
+            <button class="btn-enrol-option">
+                <span class="hov-e"></span>
+                Degrees
+            </button>
+        </div>
+    </section>
+    <footer class="footer w-100">
+        <div class="upper-footer w-100 ">
+            <h3>A MASTERPIECE FOUNDATION</h3>
+            <p>@anillaschool</p>
+            <h5>FOLLOW US ON ALL INTERNET PLATFORMS</h5>
+        </div>
+        <div class="lower-footer w-75">
+            <div class="footer-list w-100">
+                <h4>CONTACT US</h4>
+                <h4>NEWS</h4>
+                <h4>LOCATION</h4>
+                <h4>FEEDBACK</h4>
+            </div>
+            <p class="w-75">© Copyright 2022 Anilla School. All rights reserved. Terms of Use and Privacy Policy.</p>
+        </div>
+    </footer>
+</body>
+
+</html>
